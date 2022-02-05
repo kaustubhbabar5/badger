a4f3fc9ec2e0f7c61cd9a3b0740087f1508b82f3
Kaustubh Babar <kaustubhbabar@kawa.space>
2022-02-05 23:24:38 +0530

    some changes to client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -15,29 +15,39 @@ func main() {
 	go worker("03")
 	go worker("04")
 	go worker("05")
+	go worker("06")
+	go worker("07")
+	go worker("08")
+	go worker("09")
+
 	//TODO use waitgroup
 	time.Sleep(time.Hour * 1)
 }
 
 func worker(id string) {
-	i := 0
+	i := 1
 	for {
 		rand.Seed(time.Now().UnixNano())
+
 		randomInt := rand.Intn(1000)
 
-		log.Println(i, "sleeping for ms:", randomInt)
+		// log.Println(i, "sleeping for ms:", randomInt)
 		time.Sleep(time.Millisecond * time.Duration(randomInt))
 
 		input := fmt.Sprintf("worker-no=%s,message-no=%d", id, i)
 
 		url := "http://127.0.0.1:8000"
 
-		res, err := http.Post(url, "string", bytes.NewBuffer([]byte(input)))
+		_, err := http.Post(url, "string", bytes.NewBuffer([]byte(input)))
 		if err != nil {
 			panic(err)
 		}
+		if i%100 == 0 {
+			time.Sleep(time.Second * time.Duration(rand.Intn(10)))
+			log.Println("working going to sleep", id)
+		}
 
-		log.Println(fmt.Sprintf("%d status, %s", res.StatusCode, input))
+		// log.Println(fmt.Sprintf("%d status, %s", res.StatusCode, input))
 
 		i++
 	}
